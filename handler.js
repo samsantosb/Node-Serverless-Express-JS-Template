75f144d21471684f4c6fe3a15fc1b09ec58a2863
samsantosb <samsantosb@outlook.com>
2022-09-21 21:02:40 -0300

    db: solved db connection

--- handler.js
+++ handler.js
@@ -1,7 +1,9 @@
 const { car } = require('./src/cars/factories/car.factory');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const serverless = require("serverless-http");
 const express = require("express");
-const { mongoConnect, mongoDisconnect } = require("./src/db/mongo.connection");
+//const { mongoConnect, mongoDisconnect } = require("./src/db/mongo.connection");
 const app = express();
 
 app.get("/", car.getAll.bind(car));
@@ -10,9 +12,23 @@ app.post("/", car.create.bind(car));
 app.put("/:id", car.update.bind(car));
 app.delete("/:id", car.delete.bind(car));
 
-const dbAndApp = () => {
-  mongoConnect();
-  return app;
-}
+let cachedDb = null;
+const uri = process.env.MONGO;
 
-module.exports.handler = serverless(dbAndApp(), { callbackWaitsForEmptyEventLoop: false });
+module.exports.handler = serverless(
+  app,
+  {
+    request: async (request, event, context) => {
+      context.callbackWaitsForEmptyEventLoop = false;
+
+      if (cachedDb === null) {
+        cachedDb = mongoose.connect(uri, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true, serverSelectionTimeoutMS: 5000
+        });
+      }
+
+      await cachedDb;
+    },
+  }
+);
